2549e1dfa472606bd721f3a1151be89d42a734ae
MabinGo <bin.ma@huawei.com>
2017-08-23 17:05:43 +0800

    Support eventloader and fix code according to PR comment
    Signed-off-by: MabinGo <bin.ma@huawei.com>

--- seckill-event-store/src/main/java/io/servicecomb/poc/demo/seckill/repositories/SpringBasedPromotionEventRepository.java
+++ seckill-event-store/src/main/java/io/servicecomb/poc/demo/seckill/repositories/SpringBasedPromotionEventRepository.java
@@ -16,6 +16,7 @@
 
 package io.servicecomb.poc.demo.seckill.repositories;
 
+import io.servicecomb.poc.demo.seckill.Promotion;
 import io.servicecomb.poc.demo.seckill.event.PromotionEvent;
 import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -25,6 +26,5 @@ public interface SpringBasedPromotionEventRepository extends PagingAndSortingRep
 
   List<PromotionEvent<String>> findByPromotionId(String promotionId);
   PromotionEvent<String> findTopByPromotionIdAndTypeOrderByIdDesc(String couponId, String type);
-  List<PromotionEvent> findByCustomerId(String customerId);
-  List<PromotionEvent> findByTypeOrderByTimeDesc(String eventType);
+  List<PromotionEvent> findByIdGreaterThan(int id);
 }
