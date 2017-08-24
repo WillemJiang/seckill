b1745a656e44181cfff3fc31b293072a85061ca9
MabinGo <bin.ma@huawei.com>
2017-08-24 17:00:57 +0800

    JAV-254, 274: Add unit test and clear warnnings
    Signed-off-by: MabinGo <bin.ma@huawei.com>

--- seckill-query-service/src/main/java/io/servicecomb/poc/demo/seckill/SeckillEventLoader.java
+++ seckill-query-service/src/main/java/io/servicecomb/poc/demo/seckill/SeckillEventLoader.java
@@ -1,3 +1,19 @@
+/*
+ *   Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package io.servicecomb.poc.demo.seckill;
 
 import io.servicecomb.poc.demo.seckill.event.PromotionEvent;
@@ -12,17 +28,17 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class SeckillEventLoader<T> {
+class SeckillEventLoader<T> {
   private SpringBasedPromotionEventRepository promotionEventRepository;
   private PromotionRepository promotionRepository;
 
-  public SeckillEventLoader(SpringBasedPromotionEventRepository promotionEventRepository,
+  SeckillEventLoader(SpringBasedPromotionEventRepository promotionEventRepository,
       PromotionRepository promotionRepository){
     this.promotionEventRepository = promotionEventRepository;
     this.promotionRepository = promotionRepository;
   }
 
-  public void reloadEventsScheduler() {
+  void reloadEventsScheduler() {
     ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
     executor.scheduleWithFixedDelay(new Runnable() {
                                       int currentPromotionEventIndex = 0;
@@ -50,14 +66,14 @@ public class SeckillEventLoader<T> {
   private final Map<T,List<Coupon>> customerCoupons = new HashMap<>();
   private final List<Promotion> activePromotions = new ArrayList<>();
 
-  public List<Coupon> getCustomerCoupons(T customerId){
+  List<Coupon> getCustomerCoupons(T customerId){
     return customerCoupons.get(customerId);
   }
-  public List<Promotion> getActivePromotions(){
+  List<Promotion> getActivePromotions(){
     return activePromotions;
   }
 
-  public int reloadSuccessCoupons(int promotionEventIndex){
+  private int reloadSuccessCoupons(int promotionEventIndex){
     List<PromotionEvent> promotionEvents = promotionEventRepository.findByIdGreaterThan(promotionEventIndex);
 
     for (PromotionEvent promotionEvent : promotionEvents) {
@@ -81,13 +97,13 @@ public class SeckillEventLoader<T> {
     return promotionEventIndex;
   }
 
-  public int reloadActivePromotions (int promotionEventIndex) {
+  private int reloadActivePromotions (int promotionEventIndex) {
     List<PromotionEvent> promotionEvents = promotionEventRepository.findByIdGreaterThan(promotionEventIndex);
 
     for (Promotion activePromotion : activePromotions) {
       for (PromotionEvent promotionEvent : promotionEvents) {
-        if((activePromotion.getPromotionId() == promotionEvent.getPromotionId())
-            && (promotionEvent.getType() == PromotionEventType.Finish)){
+        if(activePromotion.getPromotionId().equals(promotionEvent.getPromotionId())
+            && promotionEvent.getType().equals(PromotionEventType.Finish)) {
           activePromotions.remove(activePromotion);
         }
       }
@@ -102,8 +118,8 @@ public class SeckillEventLoader<T> {
         PromotionEvent finishEvent = promotionEventRepository.findTopByPromotionIdAndTypeOrderByIdDesc(
             currentPromotionId, PromotionEventType.Finish);
         if(finishEvent == null){
-          Promotion activepromotion = promotionRepository.findTopByPromotionId(currentPromotionId);
-          activePromotions.add(activepromotion);
+          Promotion activePromotion = promotionRepository.findTopByPromotionId(currentPromotionId);
+          activePromotions.add(activePromotion);
         }
       }
 
