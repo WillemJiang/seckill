be939b1330bc3b7c128e44f194ff60a4a90d64a8
zhengyangyong <yangyong.zheng@huawei.com>
2017-08-28 11:46:52 +0800

    DEMO-SHOW:add docker for query servcie,improve integration test

--- integration-test/src/test/java/io/servicecomb/poc/demo/seckill/SecKillIntegrationTest.java
+++ integration-test/src/test/java/io/servicecomb/poc/demo/seckill/SecKillIntegrationTest.java
@@ -1,23 +1,26 @@
-/*
- *   Copyright 2017 Huawei Technologies Co., Ltd
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-
+///*
+// *   Copyright 2017 Huawei Technologies Co., Ltd
+// *
+// *   Licensed under the Apache License, Version 2.0 (the "License");
+// *   you may not use this file except in compliance with the License.
+// *   You may obtain a copy of the License at
+// *
+// *      http://www.apache.org/licenses/LICENSE-2.0
+// *
+// *   Unless required by applicable law or agreed to in writing, software
+// *   distributed under the License is distributed on an "AS IS" BASIS,
+// *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// *   See the License for the specific language governing permissions and
+// *   limitations under the License.
+// */
+//
 package io.servicecomb.poc.demo.seckill;
 
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -27,7 +30,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.servicecomb.poc.demo.seckill.dto.CouponDto;
 import io.servicecomb.poc.demo.seckill.dto.PromotionDto;
+import io.servicecomb.poc.demo.seckill.repositories.PromotionRepository;
+import io.servicecomb.poc.demo.seckill.repositories.SpringBasedPromotionEventRepository;
 import java.util.Date;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,13 +55,25 @@ public class SecKillIntegrationTest {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
+  private PromotionRepository promotionRepository;
+
+  @Autowired
+  private SpringBasedPromotionEventRepository<String> promotionEventRepository;
+
+  @Before
+  public void setUp() throws Exception {
+    promotionEventRepository.deleteAll();
+    promotionRepository.deleteAll();
+  }
+
   @Test
   public void createPromotionAndGrabSuccessfully() throws Exception {
     MvcResult result = mockMvc.perform(post("/admin/promotions/").contentType(APPLICATION_JSON)
         .content(toJson(new PromotionDto(5, 0.7f, new Date()))))
         .andExpect(status().isOk()).andReturn();
 
-    Thread.sleep(2000);
+    Thread.sleep(1000);
 
     String promotionId = result.getResponse().getContentAsString();
 
@@ -63,6 +81,14 @@ public class SecKillIntegrationTest {
         .content(toJson(new CouponDto<>(promotionId, "zyy"))))
         .andExpect(status().isOk()).andExpect(content().string("Request accepted"));
 
+    Thread.sleep(1000);
+
+    mockMvc.perform(get("/query/coupons/{customerId}", "zyy").contentType(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().string(
+            allOf(
+                containsString(promotionId),
+                containsString("zyy"))));
   }
 
   @Test
@@ -77,7 +103,7 @@ public class SecKillIntegrationTest {
         .andExpect(status().isOk()).andReturn();
     String promotionId2 = result.getResponse().getContentAsString();
 
-    Thread.sleep(2000);
+    Thread.sleep(1000);
 
     mockMvc.perform(post("/command/coupons/").contentType(APPLICATION_JSON)
         .content(toJson(new CouponDto<>(promotionId1, "zyy"))))
@@ -86,6 +112,16 @@ public class SecKillIntegrationTest {
     mockMvc.perform(post("/command/coupons/").contentType(APPLICATION_JSON)
         .content(toJson(new CouponDto<>(promotionId2, "zyy"))))
         .andExpect(status().isOk()).andExpect(content().string("Request accepted"));
+
+    Thread.sleep(1000);
+
+    mockMvc.perform(get("/query/coupons/{customerId}", "zyy").contentType(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().string(
+            allOf(
+                containsString(promotionId1),
+                containsString(promotionId2),
+                containsString("zyy"))));
   }
 
   @Test
@@ -94,7 +130,7 @@ public class SecKillIntegrationTest {
         .content(toJson(new PromotionDto(5, 0.7f, new Date()))))
         .andExpect(status().isOk()).andReturn();
 
-    Thread.sleep(2000);
+    Thread.sleep(1000);
 
     String promotionId = result.getResponse().getContentAsString();
 
