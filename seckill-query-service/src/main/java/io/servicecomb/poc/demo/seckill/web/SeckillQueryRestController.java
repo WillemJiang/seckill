ff07b35605edd04f3793ab5d91772485673bc9ed
zhengyangyong <yangyong.zheng@huawei.com>
2017-09-23 10:23:57 +0800

    JAV-326 make QueryService support ServiceComb

--- seckill-query-service/src/main/java/io/servicecomb/poc/demo/seckill/web/SeckillQueryRestController.java
+++ seckill-query-service/src/main/java/io/servicecomb/poc/demo/seckill/web/SeckillQueryRestController.java
@@ -17,9 +17,10 @@
 package io.servicecomb.poc.demo.seckill.web;
 
 import io.servicecomb.poc.demo.seckill.SecKillEventPoller;
+import io.servicecomb.poc.demo.seckill.dto.CouponInfo;
 import io.servicecomb.poc.demo.seckill.entities.CouponEntity;
 import io.servicecomb.poc.demo.seckill.entities.PromotionEntity;
-import java.util.Collection;
+import io.servicecomb.provider.rest.common.RestSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,24 +29,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@RestSchema(schemaId = "seckillQuery")
 @RestController
 @RequestMapping("/query")
 public class SeckillQueryRestController {
 
-  private static final Logger logger = LoggerFactory.getLogger(SeckillQueryRestController.class);
+    private static final Logger logger = LoggerFactory.getLogger(SeckillQueryRestController.class);
 
-  @Autowired
-  private SecKillEventPoller<String> secKillEventPoller;
+    @Autowired
+    private SecKillEventPoller<String> secKillEventPoller;
 
-  @RequestMapping(method = RequestMethod.GET, value = "/coupons/{customerId}")
-  public Collection<CouponEntity<String>> querySuccess(@PathVariable("customerId") String customerId) {
-    logger.info("Query customer id = {} coupons", customerId);
-    return secKillEventPoller.getCustomerCoupons(customerId);
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/coupons/{customerId}")
+    public Collection<CouponInfo> querySuccess(@PathVariable("customerId") String customerId) {
+        logger.info("Query customer id = {} coupons", customerId);
+        Collection<CouponEntity<String>> coupons = secKillEventPoller.getCustomerCoupons(customerId);
+        return coupons.stream()
+                .map(coupon -> new CouponInfo(coupon.getId(), coupon.getPromotionId(), new Date(coupon.getTime()), coupon.getDiscount()))
+                .collect(Collectors.toList());
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/promotions")
-  public Collection<PromotionEntity> queryCurrent() {
-    logger.info("Query current active promotions");
-    return secKillEventPoller.getActivePromotions();
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/promotions")
+    public Collection<PromotionEntity> queryCurrent() {
+        logger.info("Query current active promotions");
+        return secKillEventPoller.getActivePromotions();
+    }
 }
